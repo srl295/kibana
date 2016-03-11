884201db65d2ed6737c50c3aefec80bc1ec912b6
Steven R. Loomis <srloomis@us.ibm.com>
2016-03-11 13:21:58 -0800

    hack in progress- add angular-translate

--- webpackShims/angular.js
+++ webpackShims/angular.js
@@ -3,5 +3,6 @@ require('node_modules/angular/angular');
 module.exports = window.angular;
 
 require('node_modules/angular-elastic/elastic');
+require('node_modules/angular-translate/dist/angular-translate');
 
 require('ui/modules').get('kibana', ['monospaced.elastic']);
